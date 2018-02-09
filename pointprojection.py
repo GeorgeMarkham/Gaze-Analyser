d728104ea8fa659ab9953407951ddf37be01e4d5
George Markham <markhamg7@gmail.com>
2018-02-09 09:43:22 +0000

    update

--- pointprojection.py
+++ pointprojection.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from math import atan
 
 with np.load('calib_data.npz') as data:
-    K = np.array(data['mtx'])
+    #K = np.array(data['mtx'])
+    K = [[ 282.363047,      0.,          166.21515189],[   0.,          280.10715905,  108.05494375],[   0.,            0.,            1.        ]]
     sx = K[0][0]
     sy = K[1][1]
     cx = K[0][2]
@@ -13,15 +14,21 @@ with np.load('calib_data.npz') as data:
 
     print('sx: ', sx, '\nsy: ', sy, '\n\ncx: ', cx, '\ncy: ', cy)
 
-    x = 200
-    y = 200
+    x = 10
+    y = 10
     x_ext = ((x-cx)/sx)
     y_ext = ((y-cy)/sy)
 
-    print('coords: (', x_ext, ', ', y_ext, ', 1)')
-    
-    C = np.array([[166.21515189],[108.05494375],[1]])
-    coords = np.array([10,10,1])
-    x_ext_2 = 10/C
+    coords = np.array([x_ext, y_ext, 1])
+    coords = np.hstack(coords)
+    print(coords)
 
-    print(x_ext_2)
+    coords_world = np.linalg.inv(K)*coords
+
+    print(coords_world)
+
+    l = 1
+
+    x_ext_2 = x_ext + (l*cx)
+    y_ext_2 = y_ext + (l*cy)
+    print(x_ext_2 , ', ', y_ext_2)
